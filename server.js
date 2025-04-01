31ef5ed358d93e6fd3c5e13fd9c6b261cbea0653
Petter <petterdouglas7@gmail.com>
2025-04-01 00:01:00 -0300

    refactor: server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const jsonServer = require("json-server");
+import jsonServer from 'json-server';
 const server = jsonServer.create();
-const router = jsonServer.router("data/db.json");
+const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
